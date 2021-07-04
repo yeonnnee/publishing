797605bf1d505404e74d389c5ac9823a15851103
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-07-05 00:20:08 +0900

    header 고치는 중

--- index.js
+++ index.js
@@ -6,29 +6,10 @@ function handleHeader() {
   const currentPage = document.getElementsByClassName('now')[0];
 
   if (scrollPosition > 0) {
-    header.style.backgroundColor = '#fff';
-    header.style.padding = "10px 0";
-    header.style.color = '#000';
-    header.style.boxShadow = "0 5px 10px -5px rgb(218, 217, 217)";
-
-    social[0].classList.add('dark-icon');
-    social[1].classList.add('dark-icon');
-    social[2].classList.add('dark-icon');
-    social[3].classList.add('dark-icon');
-
-    currentPage.style.borderBottom = '1px solid #fd7e14';
+    header.classList.add('dark-header');
 
   } else if(scrollPosition === 0){
-    header.style.backgroundColor = 'transparent';
-    header.style.padding = "30px 0";
-    header.style.color = '#fff';
-    header.style.boxShadow = "none";
-    social[0].classList.remove('dark-icon');
-    social[1].classList.remove('dark-icon');
-    social[2].classList.remove('dark-icon');
-    social[3].classList.remove('dark-icon');
-
-    currentPage.style.borderBottom = '1px solid #fff';
+    header.classList.remove('dark-header');
 
   } else {
     return;
