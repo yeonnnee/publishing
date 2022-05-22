e001d21fc3f31f32e4421ad838cade7e33c1fb9a
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-05-23 01:14:40 +0900

    프로젝트 리스트 UI 수정

--- script/index.js
+++ script/index.js
@@ -1,18 +1,52 @@
 const checkBoxArr = document.getElementsByClassName('cbx');
 
-
 function clickCheckBox(e) {
   const target = e.target;
 
+  let activedItemId = '';
   if (checkBoxArr && checkBoxArr.length > 0) {
     const checkedItem = Array.from(checkBoxArr).filter(input => input.checked);
     checkedItem.forEach(item => {
-      console.log(item.id)
       if (item.id !== target.id) {
         item.checked = false;
+      } else {
+        activedItemId = item.id;
       }
     });
   }
+
+  toggleIcon(activedItemId);
+}
+
+function showIcon(icon) {
+  icon.classList.add('show-icon');
+  icon.classList.remove('hidden-icon');
+}
+
+function hideIcon(icon) {
+  icon.classList.add('hidden-icon');
+  icon.classList.remove('show-icon');
+}
+
+function toggleIcon(activedItemId) {
+  const downIcons = document.getElementsByClassName('fa-angle-down');
+  const upIcons = document.getElementsByClassName('fa-angle-up');
+
+  Array.from(upIcons).forEach(icon => {
+    if (Array.from(icon.classList).indexOf(activedItemId) > -1) {
+      showIcon(icon);
+    } else {
+      hideIcon(icon);
+    }
+  });
+
+  Array.from(downIcons).forEach(icon => {
+    if (Array.from(icon.classList).indexOf(activedItemId) > -1) {
+      hideIcon(icon);
+    } else {
+      showIcon(icon);
+    }
+  });
 }
 
 Array.from(checkBoxArr).forEach(item => {
